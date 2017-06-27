31de0508e37dfa2f82adff454c094e675e0eb97c
Simon Etter <s.etter@warwick.ac.uk>
2017-06-27 18:19:44 +0100

    Add code

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # LogarithmicGreensFunctions
 
-[![Build Status](https://travis-ci.org/ettersi/LogarithmicGreensFunctions.jl.svg?branch=master)](https://travis-ci.org/ettersi/LogarithmicGreensFunctions.jl)
-
-[![Coverage Status](https://coveralls.io/repos/ettersi/LogarithmicGreensFunctions.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/ettersi/LogarithmicGreensFunctions.jl?branch=master)
-
-[![codecov.io](http://codecov.io/github/ettersi/LogarithmicGreensFunctions.jl/coverage.svg?branch=master)](http://codecov.io/github/ettersi/LogarithmicGreensFunctions.jl?branch=master)
+See tests for example usage. 
--- src/LogarithmicGreensFunctions.jl
+++ src/LogarithmicGreensFunctions.jl
@@ -1,5 +1,73 @@
 module LogarithmicGreensFunctions
 
-# package code goes here
+export greens,domain
+
+csqrt(z) = sqrt(complex(z))
+
+# Need to turn domains into abstract arrays to make the dot syntax work
+# (e.g. greens(domain(-1,1),[-2,0,2]) )
+abstract Domain{T} <: AbstractVector{T}
+Base.size(::Domain) = (1,)
+Base.getindex(D::Domain,i::Int) = D
+Base.linearindexing{T<:Domain}(::Type{T}) = Base.LinearFast()
+
+
+# One interval
+
+domain(a,b) = Domain1I(promote(a,b)...)
+immutable Domain1I{T} <: Domain{Domain1I{T}}
+    a::T
+    b::T
+end
+
+function greens(z)
+    s = signbit(real(z)) ? -1 : 1
+    return log(abs(z + s*csqrt(z^2-1)))
+end
+function greens(D::Domain1I,z) 
+    a,b = D.a,D.b
+    greens(2/(b-a)*(z - (b+a)/2))
+end
+
+
+# Two intervals
+
+using FastGaussQuadrature
+using StaticArrays
+
+function domain(a,b,c,d) 
+    σ = SVector(a,b,c,d)
+    Domain2I(σ,compute_s(σ))
+end
+immutable Domain2I <: Domain{Domain2I}
+    σ::SVector{4,Float64}
+    s::Float64
+end
+
+# Estimate number of quadrature points needed to reach machine precision
+# Actually, the convergence rate is 2*greens(...), but dropping the factor 
+# 2 balances that we are not estimating the prefactor
+nquad(D::Domain1I,σ) = ceil(Int,-log(eps(Float64))/minimum(greens.(D,σ)))
+
+function compute_s(σ)
+    q = nquad(domain(σ[2],σ[3]), SVector(σ[1],σ[4]))
+    x,w = gaussjacobi(q, -0.5,-0.5)
+    x = (σ[3]+σ[2])/2 + (σ[3]-σ[2])/2 * x
+    denom = sqrt(-(x-σ[1]).*(x-σ[4]))
+    return sum(w.*x./denom)/sum(w./denom)
+end
+
+function greens(D::Domain2I,z)
+    σ = D.σ
+    s = D.s
+
+    i = indmin(abs(z - σ))
+    q = nquad(domain(σ[i],z), SVector(σ[mod1(i-1,4)],σ[mod1(i+1,4)]))
+    x,w = gaussjacobi(q,0,-0.5)::Tuple{Vector{Float64},Vector{Float64}}
+    x = (z + σ[i])/2 + (z - σ[i])/2 * x
+    w *= csqrt((z - σ[i])/2)
+
+    return real(sum([w*(x-s)/prod([csqrt(x-σ[j]) for j = [1:i-1;i+1:4]]) for (x,w) in zip(x,w)]))
+end
 
 end # module
